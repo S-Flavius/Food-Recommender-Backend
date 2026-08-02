e7fafec68998ca8a20ac04e11e31c633288bf7ab
grozeageorge <grozea.george03@gmail.com>
2026-08-02 19:17:35 +0300

    feature: implement restaurant entity

--- domain/src/restaurant.rs
+++ domain/src/restaurant.rs
@@ -1,5 +1,9 @@
 use uuid::Uuid;
 
+/// Represents the restaurant entity.
+///
+/// A restaurant has an Uuid `id`, a `name` and a `location`.
+/// It is a data container and does not contain any deep logic.
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub struct Restaurant {
     id: Uuid,
@@ -8,8 +12,28 @@ pub struct Restaurant {
 }
 
 impl Restaurant {
-    pub fn new(id: Uuid, name: String, location: String) -> Restaurant {
-        Self { id, name, location }
+    /// Creates a new `Restaurant`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use uuid::Uuid;
+    /// use domain::restaurant::{Restaurant};
+    ///
+    /// let name = String::from("SushiPlace");
+    /// let location = String::from("Japan");
+    /// let id = Uuid::new_v4();
+    ///
+    /// let restaurant = Restaurant::try_new(id, name, location);
+    ///
+    /// assert!(restaurant.is_ok());
+    /// ```
+    pub fn try_new(id: Uuid, name: String, location: String) -> Result<Self, &'static str> {
+        if name.trim().is_empty() || location.trim().is_empty() {
+            return Err("Name or location cannot be empty");
+        }
+
+        Ok(Self { id, name, location })
     }
 
     pub fn id(&self) -> Uuid {
@@ -23,13 +47,12 @@ impl Restaurant {
     pub fn location(&self) -> &str {
         &self.location
     }
-
 }
 
 #[cfg(test)]
 mod tests {
-    use uuid::Uuid;
     use super::*;
+    use uuid::Uuid;
 
     #[test]
     fn should_create_restaurant() {
@@ -39,11 +62,41 @@ mod tests {
         let location = String::from("Bucharest, Romania");
 
         // Act
-        let restaurant = Restaurant::new(id, name.clone(), location.clone());
+        let restaurant = Restaurant::try_new(id, name.clone(), location.clone());
 
         // Assert
+        assert!(restaurant.is_ok());
+        let restaurant = restaurant.unwrap();
         assert_eq!(restaurant.id(), id);
         assert_eq!(restaurant.name(), name);
         assert_eq!(restaurant.location(), location);
+    }
+
+    #[test]
+    fn should_fail_on_empty_name() {
+        // Arrange
+        let id = Uuid::new_v4();
+        let name = String::from("");
+        let location = String::from("France");
+
+        // Act
+        let restaurant = Restaurant::try_new(id, name.clone(), location.clone());
+
+        // Assert
+        assert!(restaurant.is_err());
+    }
+
+    #[test]
+    fn should_fail_on_empty_location() {
+        // Arrange
+        let id = Uuid::new_v4();
+        let name = String::from("McDonald's");
+        let location = String::from(" ");
+
+        // Act
+        let restaurant = Restaurant::try_new(id, name.clone(), location.clone());
+
+        // Assert
+        assert!(restaurant.is_err());
     }
 }
